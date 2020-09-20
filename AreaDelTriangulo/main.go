53d0e222052a95de0ed137881c4cb4c170fc790c
Ricardo Joel Gonzalez Velasco <70970994+ricardgo403@users.noreply.github.com>
2020-09-19 22:52:48 -0500

    fix(AreaDelTriangulo): Main function calculates a triangle area. So the user may enter the base and the height.

--- AreaDelTriangulo/main.go
+++ AreaDelTriangulo/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Triángulo")
+	var base float64
+	var altura float64
+	fmt.Println("Área del Triángulo")
+	fmt.Print("Ingrese la base: ")
+	fmt.Scan(&base)
+	fmt.Scanln()
+	fmt.Print("Ingrese la altura: ")
+	fmt.Scanf("%f", &altura)
+
+	output := (base * altura) / 2
+	fmt.Println("El área del triángulo es: ", output)
 }
