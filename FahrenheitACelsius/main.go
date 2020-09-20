d07f63b8072e2c35b239a43aeb29b0e17ccc7aec
Ricardo Joel Gonzalez Velasco <70970994+ricardgo403@users.noreply.github.com>
2020-09-19 23:04:11 -0500

    fix(FahrenheitACelsius): Main calculates fahrenheit degrees to celsius degrees.

--- FahrenheitACelsius/main.go
+++ FahrenheitACelsius/main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Fahrenheit a Celsius")
+	var gradosFahrenheit float64
+	fmt.Println("Área del Círculo")
+	fmt.Print("Ingrese los grados fahrenheit (°F): ")
+	fmt.Scanf("%f", &gradosFahrenheit)
+	var output float64 = (float64(5) / 9) * (gradosFahrenheit - 32)
+	fmt.Printf("Son %.2f °C\n", output)
 }
