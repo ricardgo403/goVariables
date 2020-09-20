95dc3b23621d86765dd5d59b5ae009636b41ea0d
Ricardo Joel Gonzalez Velasco <70970994+ricardgo403@users.noreply.github.com>
2020-09-19 22:37:05 -0500

    fix(AreaDelCirculo): Now the main calculates a circle's area. So the user may enter the circle's radius.

--- AreaDelCirculo/main.go
+++ AreaDelCirculo/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	fmt.Println("Círculo")
+	var radio float64
+	fmt.Println("Área del Círculo")
+	fmt.Print("Ingrese el radio: ")
+	fmt.Scanf("%f", &radio)
+
+	output := math.Pow(radio, float64(2)) * math.Pi
+	fmt.Println("El área del círculo es: ", output)
 }
